7d4018ec19071faa0344255fa7f0fe76ae8fd72f
DianaBianca <dianabiancasr@gmail.com>
2025-09-16 21:39:33 -0300

    WIP: correcao de lambdas funcionais

---
+++ lambdas/dist/auth-user/services/cognitoService.js
@@ -0,0 +1,54 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CognitoService = void 0;
+const client_cognito_identity_provider_1 = require("@aws-sdk/client-cognito-identity-provider");
+class CognitoService {
+    client;
+    userPoolId;
+    constructor() {
+        this.client = new client_cognito_identity_provider_1.CognitoIdentityProviderClient({
+            region: process.env.AWS_REGION || "us-east-2",
+        });
+        this.userPoolId = process.env.COGNITO_USER_POOL_ID || "";
+    }
+    async findUserByCpf(cpf) {
+        console.log("Iniciando busca de usuário:", cpf);
+        try {
+            const command = new client_cognito_identity_provider_1.ListUsersCommand({
+                UserPoolId: this.userPoolId,
+                Filter: `username = "${cpf}"`,
+            });
+            const response = await this.client.send(command);
+            if (!response.Users || response.Users.length === 0) {
+                console.log("Usuário não encontrado. Criando...");
+                const newUser = await this.createUser(cpf);
+                return { cpf: newUser.Username || "" };
+            }
+            const user = response.Users[0];
+            return { cpf: user.Username || "" };
+        }
+        catch (err) {
+            console.error("Erro ao buscar usuário:", err);
+            throw err;
+        }
+    }
+    async createUser(cpf) {
+        console.log("Criando usuário na base de dados...");
+        try {
+            const command = new client_cognito_identity_provider_1.AdminCreateUserCommand({
+                UserPoolId: this.userPoolId,
+                Username: cpf,
+                TemporaryPassword: "Senha123!",
+                MessageAction: "SUPPRESS",
+            });
+            const response = await this.client.send(command);
+            console.log("Usuário criado com sucesso!");
+            return response.User;
+        }
+        catch (err) {
+            console.error("Erro ao criar usuário:", err);
+            throw err;
+        }
+    }
+}
+exports.CognitoService = CognitoService;
